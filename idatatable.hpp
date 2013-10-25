dd13c65b6f1d44e51507116d166adea15d761a6a
ShH <hayksh@hayksh-8440.internal.synopsys.com>
2013-10-25 21:50:39 +0400

    Fix

--- idatatable.hpp
+++ idatatable.hpp
@@ -1,14 +1,8 @@
 #include <QGlobal>
 
-// this is my change :)
-
 class IDataTable
 {
-<<<<<<< HEAD
-public: // Public  methods section
-=======
-private:
->>>>>>> 3895cef8f1fc14de08cd25990da9ccf145a295f0
+public: 
 		virtual int getColumnCount() = 0;
 	
-}
+};
