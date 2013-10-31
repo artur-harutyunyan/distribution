282d3c4a8e3c70bebbdb6d8bebb4ebc4258f3684
ShH <shh@openmailbox.org>
2013-10-31 16:36:58 +0400

    Done parseing & table construction. Fixed compile errors. Tested. Added TODOs.

--- cdataparser.cpp
+++ cdataparser.cpp
@@ -1,41 +1,90 @@
-#include "cdataparser.h"
+gH#include "cdataparser.hpp"
 
 #include <QStringList>
 #include <QFile>
 #include <QTextStream>
+#include <QVector>
+
+#include <iostream>
 
 
-const char* CCSVParser::cszError_Open_Failure[] = "Could not open file";
-const char* CCSVParser::cszError_No_Header_Data[] = "No header data in file";
+const char CCSVParser::cszError_Open_Failure[] = "Could not open file";
+const char CCSVParser::cszError_No_Header_Data[] = "No header data in file";
 
 CCSVParser::CCSVParser(const QString& csv)
-	: m_data_table(0)
-	, m_file_name(csv)
+	// TODO swap commented lines with uncommented
+	//: m_data_table(0)
+	//, m_file_name(csv)
+	: m_file_name(csv)
 {}
+
+void addRowToTable(QStringList& r, QVector<QStringList>& t)
+{
+	int n = t.size();
+	Q_ASSERT(n == r.size());
+	for(int i = 0; i < n; ++i) {
+		t[i].push_back(r[i]);
+	}
+}
 
 void CCSVParser::parse()
 {
 	QFile f(m_file_name);
 	if(!f.open(QIODevice::ReadOnly)) {
-		throw runtime_error(cszError_Open_Failure);
+		throw ParserError(cszError_Open_Failure);
 	}
-	QTextStream ts(f);
+	QTextStream ts(&f);
 	QString line = ts.readLine();
 	if(0 == line.size()) {
-		throw runtime_error(cszError_No_Header_Data);
+		throw ParserError(cszError_No_Header_Data);
 	}
 	QStringList header = line.split(',');
-	m_data_table = new CDataTable;
-	while(!f.eof())
+	QVector<QStringList> table(header.size());
+	//m_data_table = new CDataTable;
+	while(!f.atEnd())
 	{
+		// TODO check \" case
+		// TODO Check for errors like row.size() != header.size(); and throw ParserError
 		line = ts.readLine();
-		QStringList
+		QStringList row = line.split(',');
+		addRowToTable(row, table);
+	}
+	for(int i = 0; i < table.size(); ++i) {
+		ColType t = getColType(table[i]);
+		// TODO Something like this
+		// CDataColumn c = new CDataColumn(ColType, table[i], header[i]);
+		// m_data_table.addCol(c);
 	}
 
+	// TODO For testing... Remove this
+	//for(int i = 0; i < table.size(); ++i )
+	//{
+	//	for(int j = 0; j < 10; ++j) {
+	//		std::cout << table[i][j].toStdString() << " ";
+	//	}
+	//	std::cout << std::endl << std::endl;
+	//}
+}
+
+ColType CCSVParser::getColType(const QStringList& col)
+{
+	// TODO Not yet implemented
 }
 
 IDataTable* CCSVParser::getDataTable()
 {
-	Q_ASSERT(0 == m_data_table);
-	return m_data_table;
+	//Q_ASSERT(0 == m_data_table);
+	//return m_data_table;
+	return 0;
 }
+
+/*
+	// TODO For testing... Remove this
+int main()
+{
+	CCSVParser p("../dtf/ParameterData.Pivot.csv");
+	p.parse();
+	return 0;
+}
+
+*/
--- cdataparser.hpp
+++ cdataparser.hpp
@@ -5,13 +5,39 @@
 
 #include <QString>
 
+
 #include <exception>
+#include <string>
+
+class ParserError : public std::exception
+{
+	// TODO add format
+	std::string m_message;
+	public:
+		ParserError(const std::string& s)
+			: m_message(s)
+		{}
+		const char* what()
+		{
+			return m_message.c_str();
+		}
+		~ParserError() throw()
+		{}
+};
 
 
 class CCSVParser
 {
-	static const char* cszError_Open_Failure[];
-	static const char* cszError_No_Header_Data[];
+	// TODO Temporary to pass compilation... Remove this
+	enum ColType {
+		BOOL,
+		INT,
+		FLOAT,
+		DATE,
+		STRING
+	} 
+	static const char cszError_Open_Failure[];
+	static const char cszError_No_Header_Data[];
 public:
 	/** 
 	 * @brief CCSVParser constructor.
@@ -30,7 +56,8 @@ public:
 	 */
 	IDataTable* getDataTable();
 private:
-	CDataTable* m_data_table;
+	ColType getColType(cosnt QStringList& col);
+	//CDataTable* m_data_table;
 	const QString m_file_name;
 };
 
