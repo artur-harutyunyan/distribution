447b1c15dfda2905dc4e2a1da5a38c26a4a55b4c
Tigran <tikosah@gmail.com>
2013-11-01 09:15:52 +0300

    Update cdistribution.hpp

--- cdistribution.hpp
+++ cdistribution.hpp
@@ -1,1 +1,25 @@
+#ifndef CDISTRIBUTIONMODULE_H
+#define CDISTRIBUTIONMODULE_H
+
+#include <QObject>
+
 #include <cmodule.hpp>
+#include "CAnalysisTableModel.h"
+
+namespace da
+{
+
+class CDistributionModule : public QObject
+{
+	Q_OBJECT
+public:
+
+private:
+	IDataTable* table;
+
+	CAnalysisTableModel* tableModel;
+};//class CDistributionModule
+
+}// namespace da
+
+#endif //CDISTRIBUTIONMODULE_H
