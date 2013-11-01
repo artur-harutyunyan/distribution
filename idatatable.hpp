5891d2d0e8f0452639707cfa7349377c82fb3733
Tigran <tikosah@gmail.com>
2013-11-01 09:21:46 +0300

    Update idatatable.hpp

--- idatatable.hpp
+++ idatatable.hpp
@@ -1,8 +1,23 @@
+#ifndef IDATATABLE_H
+#define IDATATABLE_H
+
 #include <QGlobal>
+
+#include "iDataColumn.h"
+
+namespace da
+{
 
 class IDataTable
 {
-public: 
-		virtual int getColumnCount() = 0;
-	
-};
+public:
+	virtual IDataColumn* getColumn() = 0;
+	virtual int getColumnCount() = 0;
+	virtual QString getColumnName() = 0;
+	virtual int getRowCount() = 0;
+	QStringList getColumnNames();
+}; // class DataColumn
+
+}// namespace da
+
+#endif // IDATATABLE_H
