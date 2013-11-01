f556438c489ad6725484998d4e3dda90d180104b
Tigran <tikosah@gmail.com>
2013-11-01 09:23:56 +0300

    Update cdatatable.hpp

--- cdatatable.hpp
+++ cdatatable.hpp
@@ -1,1 +1,16 @@
+
+#ifndef CDATATABLE
+#define  CDATATABLE
+
 #include "idatatable.hpp"
+
+namespace da
+{
+
+class CDataTable : public IDataTable
+{
+public:
+};// class CDataTable
+}// namespace da
+
+#endif // CDATATABLE
