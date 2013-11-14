0c09234a8cbf4ce77c727542b67a11df9c9fc78a
Tigran <tikosah@gmail.com>
2013-11-15 00:25:30 +0300

    Create cdatatable.cpp

---
+++ cdatatable.cpp
@@ -0,0 +1,26 @@
+#include "cdatatable.hpp"
+
+namespace da
+{
+
+IDataColumn* CDataTable::getColumn(int index)
+{
+	return m_data.at(index);
+}
+
+int CDataTable::getColumnCount()
+{
+	return m_data.size();
+}
+
+QString CDataTable::getColumnName(int index)
+{
+	return m_data.at(index)->getName();
+}
+
+int CDataTable::getRowCount()
+{
+	return m_data.at(0)->getSize();
+}
+
+} // namespace da
