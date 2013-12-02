0c48d5c74ea5dd47dd7a4f267e19116ef17f8cd6
hhovhannes <harutyunyan-hovhannes@mail.ru>
2013-12-03 01:23:48 +0400

    Initialized CMainWindow class

--- cmainwindow.hpp
+++ cmainwindow.hpp
@@ -1,1 +1,13 @@
+#ifndef CMAINWINDOW_HPP
+#define CMAINWINDOW_HPP
+
 #include <QMainWindow>
+
+class CMainWindow : public QMainWindow
+{
+public:
+	CMainWindow() : QMainWindow(//??? )
+	{}
+}
+
+#endif //CMAINWINDOW_HPP
