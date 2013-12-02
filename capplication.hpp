141fac271a40b7d95193dc77b208aa35663b9df3
hhovhannes <harutyunyan-hovhannes@mail.ru>
2013-12-03 01:22:43 +0400

    Initialized CMainWindow class.

--- capplication.hpp
+++ capplication.hpp
@@ -1,1 +1,19 @@
+#ifndef CAPPLICATION_HPP
+#define CAPPLICATION_HPP
+
 #include <QApplication>
+
+class CApplication : public QApplication
+{
+public:
+	CApplication( int argc, char** argv) : QApplication( argc, argv )
+	{}
+
+	getModule()
+	{
+		return //something;
+	}
+
+}
+
+#endif //CAPPLICATION_HPP
